544dec788f3d31e93abbfe4ee0dbaeaa1e065b1d
Dell <Dell@DESKTOP-NGPN7OV>
2022-05-15 15:24:44 -0400

    test

--- controller/guardarUsuarios.py
+++ controller/guardarUsuarios.py
@@ -4,11 +4,4 @@ Created on 09-05-2022
 @author: Dell
 '''
 
-from model.claseUI import listaInputsTxtCtrl
-
-usuariosGuardados=open("usuariosGuardados.txt","w")
-
-for i in range(0,len(listaInputsTxtCtrl),1):
-    usuariosGuardados.write(listaInputsTxtCtrl[i]+"\n")
-
-usuariosGuardados.close()
+#esto va a ser completamente modificado para aplicar programación orientada a objetos
