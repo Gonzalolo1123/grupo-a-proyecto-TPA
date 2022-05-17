9adec1687bc6c08cb7b58fc6bbacb5042fb996b4
Gonzalolo1123 <gonzalolorenzo69@gmail.com>
2022-05-17 11:05:42 -0400

    HU-001-TP-001 Creación de grilla: mainGrilla con imágenes en proceso

--- view/mainGrilla.py
+++ view/mainGrilla.py
@@ -5,28 +5,62 @@ Created on 10-05-2022
 '''
 import wx
 import wx.grid as wxgrid
-from random import randint
-filas = 4
-columnas = 4
+filas = 5
+columnas = 10
+dsa=columnas*filas
 class TestFrame(wx.Frame):
     def __init__(self,parent,title):
-            wx.Frame.__init__(self,parent=parent,title=title)
-            self.grid = wxgrid.Grid(self, -1)
+            frame=wx.Frame.__init__(self,parent=parent,title=title,size=(1100,800))
+            panel=wx.Panel(self,-1)
+            self.grid = wxgrid.Grid(panel, -1,size=(1100,800))
             self.grid.CreateGrid(filas,columnas)
+
+            contFilas = 0
+            contColumnas = 0
+            for i in range(0,dsa,1):
+                img = wx.Bitmap("Sin_carta.jpg", wx.BITMAP_TYPE_ANY)
+                img = self.scale_bitmap(img, 100, 150)
+                imageRenderer = MyImageRenderer(img)
+                if contFilas<filas:
+                    self.grid.SetCellRenderer(contFilas,i%10, imageRenderer)
+                    self.grid.SetColSize(i%10, img.GetWidth() + 2)
+                    self.grid.SetRowSize(contFilas, img.GetHeight() + 2)
+                    if i==columnas:
+                        contFilas=+1
+            self.Centre(True)
+            self.Show()
             self.grid.SetRowLabelSize(0)
+            self.grid.SetColLabelSize(0)
+    def scale_bitmap(self,bitmap, width, height):
+        image = wx.ImageFromBitmap(bitmap)
+        image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+        result = wx.BitmapFromImage(image)
+        return result
 
-
-        # Modificando los encabezados y anchos de columna
-            anchos = [100, 100,100,100]
-            encabezados = ["", "","",""]
-            for n, col in enumerate(range(columnas)):
-                self.grid.SetColLabelValue(col, encabezados[n])
-                self.grid.SetColSize(col, anchos[n])
-
-                self.Centre(True)
-                self.Show()
+class MyImageRenderer(wx.grid.GridCellRenderer):
+    def __init__(self, img):
+        wx.grid.GridCellRenderer.__init__(self)
+        self.img = img
+    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+        image = wx.MemoryDC()
+        image.SelectObject(self.img)
+        dc.SetBackgroundMode(wx.SOLID)
+        if isSelected:
+            dc.SetBrush(wx.Brush(wx.BLUE, wx.SOLID))
+            dc.SetPen(wx.Pen(wx.BLUE, 1, wx.SOLID))
+        else:
+            dc.SetBrush(wx.Brush(wx.WHITE, wx.SOLID))
+            dc.SetPen(wx.Pen(wx.WHITE, 1, wx.SOLID))
+        dc.DrawRectangle(rect)
+        width, height = self.img.GetWidth(), self.img.GetHeight()
+        if width > rect.width-2:
+            width = rect.width-2
+        if height > rect.height-2:
+            height = rect.height-2
+        dc.Blit(rect.x+1, rect.y+1, width, height, image, 0, 0, wx.COPY, True)
 
 if __name__=='__main__':
     app = wx.App()
     fr = TestFrame(None, "Test Grid")
+    fr.Show()
     app.MainLoop()
