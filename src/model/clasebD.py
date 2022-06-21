258a821e4e93db1642b2372f62c42e333fa660d0
Dell <Dell@DESKTOP-NGPN7OV>
2022-06-21 01:17:52 -0400

    HU-006-TP-001 Selección de niveles en interfaz: documentacion

--- src/model/clasebD.py
+++ src/model/clasebD.py
@@ -7,9 +7,14 @@ import wx
 
 class botonesDificultad(wx.Frame):
     '''
-    Estos son los botones dificultad
+    Esta es la clase de los botones de dificultad
     '''
     def __init__(self,parent,title):
+        '''
+        :parent: Es el padre de la ventana.
+        :title: Es el nombre de la ventana.
+        '''
+        
         wx. Frame.__init__(self, parent,title=title, size=(300,250))
         self.boton1 = wx.Button(self, 0, "Facil", size=(100, 50), pos=(100,10))
         self.boton2 = wx.Button(self, 0, "Medio", size=(100, 50), pos=(100,70))
