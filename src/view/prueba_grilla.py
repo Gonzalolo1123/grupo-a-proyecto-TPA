767377fe83a19fb5d6482e4f92f22ebcbda2f895
Gonzalolo1123 <gonzalolorenzo69@gmail.com>
2022-06-22 12:12:56 -0400

    HU-001-TP-002: Coomparacion de Cartas

--- src/view/prueba_grilla.py
+++ src/view/prueba_grilla.py
@@ -1,17 +1,9 @@
 import wx
 import wx.grid as wxgrid
 import random
-
+#cambio
 filas = 4
 columnas = 4
-
-
-def scale_bitmap(bitmap, width, height):
-    image = wx.ImageFromBitmap(bitmap)
-    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
-    result = wx.BitmapFromImage(image)
-    return result
-
 
 class TestFrame(wx.Frame):
     def __init__(self, parent, title):
@@ -19,25 +11,22 @@ class TestFrame(wx.Frame):
         :parent:
         :title:
         '''
-        self.click1 = []
-        self.click2 = []
-        self.fil = None
-        self.col = None
-        self.cont = 0
+        self.click1=[]
+        self.click2=[]
+        self.cont=0
         self.matrizaux = []
         res = random.sample(range(1, 32), 8)
         res.extend(res)
         random.shuffle(res)
         self.matrizaux.append(res)
-        self.M = [self.matrizaux[0][columnas * i: columnas * (i + 1)] for i in range(filas)]
-
-        frame = wx.Frame.__init__(self, parent=parent, title=title, size=(1400,700))
+        self.M = [self.matrizaux[0][columnas*i : columnas*(i+1)] for i in range(filas)]
+        
+        frame = wx.Frame.__init__(self, parent=parent, title=title, size=(500,700))
         panel = wx.Panel(self, -1)
-        grid = wxgrid.Grid(panel, -1, size=(700, 700))
-        grid.CreateGrid(filas, columnas)
         self.grid = wxgrid.Grid(panel, -1, size=(700, 700))
         self.grid.CreateGrid(filas, columnas)
-        grid.Position = (701, 0)
+        self.grid.Position = (40, 25)
+       
         # Grilla 1
         for i in range(0, columnas):
             for j in range(0, filas):
@@ -47,58 +36,56 @@ class TestFrame(wx.Frame):
                 self.grid.SetCellRenderer(j, i, imageRenderer1)
                 self.grid.SetColSize(i, img1.GetWidth() + 2)
                 self.grid.SetRowSize(j, img1.GetHeight() + 2)
+                
         # Grilla 2
         c = 0
         for f in range(0, filas):
             for a in range(0, columnas):
                 if c <= 3:
                     img2 = wx.Bitmap("cardsColors/" + str(self.matrizaux[0][c]) + ".jpg", wx.BITMAP_TYPE_ANY)
-                    img2 = scale_bitmap(img2, 100, 150)
+                    img2 = self.scale_bitmap(img2, 100, 150)
                     b = 0
                 if 7 >= c >= 4:
                     img2 = wx.Bitmap("cardsColors/" + str(self.matrizaux[0][c]) + ".jpg", wx.BITMAP_TYPE_ANY)
-                    img2 = scale_bitmap(img2, 100, 150)
+                    img2 = self.scale_bitmap(img2, 100, 150)
                     b = 1
                 if 11 >= c >= 8:
                     img2 = wx.Bitmap("cardsColors/" + str(self.matrizaux[0][c]) + ".jpg", wx.BITMAP_TYPE_ANY)
-                    img2 = scale_bitmap(img2, 100, 150)
+                    img2 = self.scale_bitmap(img2, 100, 150)
                     b = 2
                 if 15 >= c >= 12:
                     img2 = wx.Bitmap("cardsColors/" + str(self.matrizaux[0][c]) + ".jpg", wx.BITMAP_TYPE_ANY)
-                    img2 = scale_bitmap(img2, 100, 150)
+                    img2 = self.scale_bitmap(img2, 100, 150)
                     b = 3
                 c += 1
                 imageRenderer2 = MyImageRenderer(img2)
-                grid.SetCellRenderer(b, a, imageRenderer2)
-                grid.SetColSize(a, img2.GetWidth() + 2)
-                grid.SetRowSize(b, img2.GetHeight() + 2)
+                self.grid.SetCellRenderer(b, a, imageRenderer2)
+                self.grid.SetColSize(a, img2.GetWidth() + 2)
+                self.grid.SetRowSize(b, img2.GetHeight() + 2)
 
-            self.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.click)
+            self.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK,self.click)
             self.Centre(True)
             self.Show()
             self.grid.SetRowLabelSize(0)
             self.grid.SetColLabelSize(0)
-            grid.SetRowLabelSize(0)
-            grid.SetColLabelSize(0)
-            self.grid.EnableCellEditControl
         print(self.M)
-
+        
         # PARTE COMPARAR
-        self.elementosComparados = []
-        self.coordenadasElementosIguales = []
-        longitudMatriz = len(self.M)
+        self.elementosComparados=[]
+        self.coordenadasElementosIguales=[]
+        longitudMatriz=len(self.M)
 
-        for j in range(0, longitudMatriz, 1):
-            for k in range(0, longitudMatriz, 1):
-                for u in range(0, longitudMatriz, 1):
-                    for h in range(0, longitudMatriz, 1):
+        for j in range(0,longitudMatriz,1):
+            for k in range(0,longitudMatriz,1):
+                for u in range(0,longitudMatriz,1):
+                    for h in range(0,longitudMatriz,1):
                         if str(self.M[j][k]) in self.elementosComparados:
                             pass
                         else:
                             if self.M[j][k] == self.M[u][h] and j != u and k != h:
                                 self.elementosComparados.append(str(self.M[j][k]))
-                                coordIguales1 = str(j) + str(k) + str(u) + str(h)
-                                coordIguales2 = str(u) + str(h) + str(j) + str(k)
+                                coordIguales1=str(j)+str(k)+str(u)+str(h)
+                                coordIguales2=str(u)+str(h)+str(j)+str(k)
                                 self.coordenadasElementosIguales.append(coordIguales1)
                                 self.coordenadasElementosIguales.append(coordIguales2)
 
@@ -109,35 +96,36 @@ class TestFrame(wx.Frame):
     def click(self, event):
         self.col = event.GetCol()
         self.fil = event.GetRow()
-        self.cont += 1
-        if self.cont == 1:
+        self.cont+=1
+        if self.cont==1:
             self.click1.append(self.fil)
             self.click1.append(self.col)
-
-        if self.cont == 2:
+            
+        if self.cont==2:
             self.click2.append(self.fil)
             self.click2.append(self.col)
-            self.cont = 0
-
+            self.cont=0
+            
             # COMPARACION POSICIONES CLICK
-            coordComparar = str(self.click1[0]) + str(self.click1[1]) + str(self.click2[0]) + str(self.click2[1])
+            coordComparar=str(self.click1[0])+str(self.click1[1])+str(self.click2[0])+str(self.click2[1])
             if str(coordComparar) in self.coordenadasElementosIguales:
                 print("iguales")
             else:
                 print("diferentes")
-                self.grid.DeselectCol(self.col)
-                self.grid.DeselectRow(self.fil)
             # FIN POSICIONES CLICK
+            
+            self.click1=[]
+            self.click2=[]
 
-            self.click1 = []
-            self.click2 = []
-
-
+    def scale_bitmap(self,bitmap, width, height):
+        image = wx.ImageFromBitmap(bitmap)
+        image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+        result = wx.BitmapFromImage(image)
+        return result
 class MyImageRenderer(wx.grid.GridCellRenderer):
     def __init__(self, img):
         wx.grid.GridCellRenderer.__init__(self)
         self.img = img
-
     def Draw(self, grid, attr, dc, rect, row, col, isSelected):
         image = wx.MemoryDC()
         image.SelectObject(self.img)
@@ -150,14 +138,12 @@ class MyImageRenderer(wx.grid.GridCellRenderer):
             dc.SetPen(wx.Pen(wx.WHITE, 1, wx.SOLID))
         dc.DrawRectangle(rect)
         width, height = self.img.GetWidth(), self.img.GetHeight()
-        if width > rect.width - 2:
-            width = rect.width - 2
-        if height > rect.height - 2:
-            height = rect.height - 2
-        dc.Blit(rect.x + 1, rect.y + 1, width, height, image, 0, 0, wx.COPY, True)
-
-
-if __name__ == '__main__':
+        if width > rect.width-2:
+            width = rect.width-2
+        if height > rect.height-2:
+            height = rect.height-2
+        dc.Blit(rect.x+1, rect.y+1, width, height, image, 0, 0, wx.COPY, True)
+if __name__=='__main__':
     app = wx.App()
     fr = TestFrame(None, "Test Grid")
     fr.Show()
