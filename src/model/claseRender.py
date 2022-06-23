e18d61b93c57777dcc2e9786a75f9ef69850c87d
Dell <Dell@DESKTOP-NGPN7OV>
2022-06-22 21:13:58 -0400

    HU-001-TP-001 Creación de grilla: clase MyImageRender aparte

---
+++ src/model/claseRender.py
@@ -0,0 +1,37 @@
+'''
+Created on 22-06-2022
+
+@author: Dell
+'''
+
+import wx
+import wx.grid as wxgrid
+
+def scale_bitmap(bitmap, width, height):
+    image = wx.ImageFromBitmap(bitmap)
+    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+    result = wx.BitmapFromImage(image)
+    return result
+
+class MyImageRenderer(wxgrid.GridCellRenderer):
+    def __init__(self, img):
+        wxgrid.GridCellRenderer.__init__(self)
+        self.img = img
+
+    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+        image = wx.MemoryDC()
+        image.SelectObject(self.img)
+        dc.SetBackgroundMode(wx.SOLID)
+        if isSelected:
+            dc.SetBrush(wx.Brush(wx.BLUE, wx.SOLID))
+            dc.SetPen(wx.Pen(wx.BLUE, 1, wx.SOLID))
+        else:
+            dc.SetBrush(wx.Brush(wx.WHITE, wx.SOLID))
+            dc.SetPen(wx.Pen(wx.WHITE, 1, wx.SOLID))
+        dc.DrawRectangle(rect)
+        width, height = self.img.GetWidth(), self.img.GetHeight()
+        if width > rect.width - 2:
+            width = rect.width - 2
+        if height > rect.height - 2:
+            height = rect.height - 2
+        dc.Blit(rect.x + 1, rect.y + 1, width, height, image, 0, 0, wx.COPY, True)
